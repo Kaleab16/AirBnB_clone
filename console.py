249321cc0028f232e88e5ead6ac14c20ba97afa5
Kaleab16 <classkaleab@gmail.com>
2026-09-16 11:12:22 +0300

    Add command interpreter console

--- console.py
+++ console.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """Command interpreter module."""
 
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the AirBnB project."""
+
+    prompt = '(hbnb)'
+
+    def do_quit(self,arg):
+        """Quit command to exist the program."""
+        print()
+        return True
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
 
 if __name__ == "__main__":
-    pass
+    HBNBCommand().cmdloop()
